f04517f1870658639ee582594a71e00257dae906
JonahCysner <jonahc18@nycstudents.net>
2025-11-20 15:19:46 -0500

    Finish

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args){
+        FrogSimulation Froggy = new FrogSimulation(24, 5);
+        System.out.println(Froggy.simulate());
+        System.out.println(Froggy.runSimulations(400));
+    }
 }
